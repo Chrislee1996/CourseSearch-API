74470794fb65435bcfc4a663ee2297d59045c5b8
chrislee1996 <prepchrislee@gmail.com>
2022-04-21 18:07:54 -0400

    update route works

--- app/routes/course_routes.js
+++ app/routes/course_routes.js
@@ -61,28 +61,25 @@ router.post('/courses', requireToken, (req, res, next) => {
 		.catch(next)
 })
 
-// // UPDATE
-// // PATCH /examples/5a7db6c74d55bc51bdf39793
-// router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
-// 	// if the client attempts to change the `owner` property by including a new
-// 	// owner, prevent that by deleting that key/value pair
-// 	delete req.body.example.owner
 
-// 	Example.findById(req.params.id)
-// 		.then(handle404)
-// 		.then((example) => {
-// 			// pass the `req` object and the Mongoose record to `requireOwnership`
-// 			// it will throw an error if the current user isn't the owner
-// 			requireOwnership(req, example)
+router.patch('/courses/:id', requireToken, removeBlanks, (req, res, next) => {
+	delete req.body.course.owner
 
-// 			// pass the result of Mongoose's `.update` to the next `.then`
-// 			return example.updateOne(req.body.example)
-// 		})
-// 		// if that succeeded, return 204 and no JSON
-// 		.then(() => res.sendStatus(204))
-// 		// if an error occurs, pass it to the handler
-// 		.catch(next)
-// })
+	Course.findById(req.params.id)
+		.then(handle404)
+		.then((course) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, course)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return course.updateOne(req.body.course)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
 
 // // DESTROY
 // // DELETE /examples/5a7db6c74d55bc51bdf39793
