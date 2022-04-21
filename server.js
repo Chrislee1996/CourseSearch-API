ec9207ce6aaf715284179193311bff3801473bb2
chrislee1996 <prepchrislee@gmail.com>
2022-04-21 17:18:06 -0400

    users can signup signin and logout

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const courseRoutes = require('./app/routes/course_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -65,7 +65,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(courseRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
