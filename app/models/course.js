2989c6ecfa6a496d673c4abf37294feea041d8a0
chrislee1996 <prepchrislee@gmail.com>
2022-04-24 21:52:37 -0400

    creating course now works

--- app/models/course.js
+++ app/models/course.js
@@ -14,7 +14,6 @@ const courseSchema = new mongoose.Schema(
 		},
         image: {
 			type: String,
-			default: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop"
 		},
         courseLink: {
             type: String,
@@ -41,6 +40,7 @@ const courseSchema = new mongoose.Schema(
 		},
         daysOfCourse: {
 			type: String,
+			required:true
 		},
         startTime: {
 			type: String,
