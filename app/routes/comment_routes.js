8d7b4d99ed868f7042201d092b95157bba008b8e
chrislee1996 <prepchrislee@gmail.com>
2022-04-25 15:59:42 -0400

    updated schema

--- app/routes/comment_routes.js
+++ app/routes/comment_routes.js
@@ -2,14 +2,13 @@
 const express = require('express')
 const passport = require('passport')
 const Review = require('../models/review')
+const Course = require('../models/course')
 const customErrors = require('../../lib/custom_errors')
 
 const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 const requireToken = passport.authenticate('bearer', { session: false })
 const removeBlanks = require('../../lib/remove_blank_fields')
-const review = require('../models/review')
-
 const router = express.Router()
 
 
@@ -17,21 +16,46 @@ const router = express.Router()
 
 
 router.post('/comments/:reviewId', (req, res, next) => {
-    // get our review from req.body
-    const comment = req.body.comment
-    // get our reviewId from req.params.id
-    const reviewId = req.params.reviewId
-    Review.findById(reviewId)
-        // handle what happens if no review is found
+    const review = req.body.review
+    const courseId = req.params.courseId
+    Course.findById(courseId)
         .then(handle404)
-        .then(review => {
-            review.comments.push(comment)
-            return review.save()
+            .then((course) => {
+                course.reviews.push(review)
+                return course.save()
+            })
+            .then(course => res.status(201).json({ course: course }))
+            // catch errors and send to the handler
+            .catch(next)
         })
-        .then(review => res.status(201).json({ review: review }))
-        // catch errors and send to the handler
-        .catch(next)
-})
+    // const comment = req.body.comment
+    // const reviewId = req.params.reviewId
+    // Review.findById(reviewId)
+    //     .then(handle404)
+    //         .then((review) => {
+    //             review.comments.push(comment)
+    //             return review.save()
+    //         })
+    //         .then(review => res.status(201).json({ review: review }))
+    //         // catch errors and send to the handler
+    //         .catch(next)
+    // })
+
+//     // get our review from req.body
+//     const comment = req.body.comment
+//     // get our reviewId from req.params.id
+//     const reviewId = req.params.reviewId
+//     Review.findById(reviewId)
+//         // handle what happens if no review is found
+//         .then(handle404)
+//         .then(review => {
+//             review.comments.push(comment)
+//             return review.save()
+//         })
+//         .then(review => res.status(201).json({ review: review }))
+//         // catch errors and send to the handler
+//         .catch(next)
+// })
 
 router.delete('/comments/:reviewId/:commentId', requireToken,(req, res, next) => {
     const commentId = req.params.commentId
