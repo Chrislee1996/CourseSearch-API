b2ae75eec3d3f59b0c1c3d8cb44039d6c78367bb
chrislee1996 <prepchrislee@gmail.com>
2022-04-25 14:41:59 -0400

    updated review works

--- app/routes/review_routes.js
+++ app/routes/review_routes.js
@@ -31,22 +31,39 @@ router.post('/reviews/:courseId', (req, res, next) => {
             .catch(next)
         })
 
-router.patch('/reviews/:id', requireToken, removeBlanks, (req, res, next) => {
-	delete req.body.review.owner
-	Review.findById(req.params.id)
-		.then(handle404)
-		.then((review) => {
-			// pass the `req` object and the Mongoose record to `requireOwnership`
-			// it will throw an error if the current user isn't the owner
-			// requireOwnership(req, review)
-			// pass the result of Mongoose's `.update` to the next `.then`
-			return review.updateOne(req.body.review)
-		})
-		// if that succeeded, return 204 and no JSON
-		.then(() => res.sendStatus(204))
-		// if an error occurs, pass it to the handler
-		.catch(next)
+router.patch('/reviews/:courseId/:reviewId', requireToken, removeBlanks, (req, res, next) => {
+    const reviewId = req.params.reviewId
+    const courseId = req.params.courseId
+
+    Course.findById(courseId)
+        .then(handle404)
+        .then(course => {
+            const theReview = course.reviews.id(reviewId)
+            // requireOwnership(req, course)
+            theReview.set(req.body.review)
+
+            return course.save()
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next)
+
 })
+
+// 	delete req.body.review.owner
+// 	Review.findById(req.params.id)
+// 		.then(handle404)
+// 		.then((review) => {
+// 			// pass the `req` object and the Mongoose record to `requireOwnership`
+// 			// it will throw an error if the current user isn't the owner
+// 			// requireOwnership(req, review)
+// 			// pass the result of Mongoose's `.update` to the next `.then`
+// 			return review.updateOne(req.body.review)
+// 		})
+// 		// if that succeeded, return 204 and no JSON
+// 		.then(() => res.sendStatus(204))
+// 		// if an error occurs, pass it to the handler
+// 		.catch(next)
+// })
 
 
 // DESTROY
