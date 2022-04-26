d55763e25bc138e6ba1727a3869508134ce91b7e
chrislee1996 <prepchrislee@gmail.com>
2022-04-26 15:12:46 -0400

    added show pages for tags

--- app/routes/tags_routes.js
+++ app/routes/tags_routes.js
@@ -26,17 +26,116 @@ router.get('/tags', (req,res,next) => {
 })
 
 
-router.get('/tags/:id', (req,res,next) => {
-	Tag.findById(req.params.id)
-		.then(handle404)
-		.then((tag) => res.status(200).json({ tag: tag.toObject() }))
-		// if an error occurs, pass it to the handler
-		.catch(next)
+// router.get('/tags/:id', (req,res,next) => {
+// 	Tag.findById(req.params.id)
+// 		.then(handle404)
+// 		.then((tag) => res.status(200).json({ tag: tag.toObject() }))
+// 		// if an error occurs, pass it to the handler
+// 		.catch(next)
+// })
+
+router.get('/tags/onlinecourses',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Online Courses'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0].details})
+
+})
+
+router.get('/tags/collegecourses',async (req,res,next) =>{
+    const tag = await Tag.find({details:'College Courses'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/noncollegecourses',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Non-College Courses'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/inpersoncourses',async (req,res,next) =>{
+    const tag = await Tag.find({details:'In person Courses'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/mandatoryattendence',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Mandatory Attendence'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/homework',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Lots of homework'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/testheavy',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Test Heavy'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+
+router.get('/tags/groupprojects',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Group Projects'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/feedback',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Good Feedback'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/material',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Cares About the Material'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/caring',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Caring'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/textbook',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Text-book Mandatory'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/toughgrader',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Tough Grader'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
+})
+
+router.get('/tags/lectureheavy',async (req,res,next) =>{
+    const tag = await Tag.find({details:'Lecture Heavy'}).populate('course')
+    console.log(tag[0], 'here is our course')
+    res.status(200).json({course:tag[0]})
+
 })
 
 
 
-router.post('/tag/:courseId', requireToken, removeBlanks, (req, res, next) => {
+router.post('/tags/:courseId', requireToken, removeBlanks, (req, res, next) => {
     const tag = req.body.tag
     const courseId = req.params.courseId
 
@@ -50,6 +149,7 @@ router.post('/tag/:courseId', requireToken, removeBlanks, (req, res, next) => {
                 })
         })
 })
+
 
 router.delete('/delete/:courseId/:tagId', requireToken, (req, res, next) => {
     const tagId = req.params.tagId
